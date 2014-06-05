d57f118bc732926bbe1f033b3c68b20969f0b8d6
Lucas Oliveira <lcastro.oliveira@gmail.com>
2014-06-05 12:47:19 +1200

    now it calls for Thye's server program put the .torrent file inside of the tracker database

--- glueupload.java
+++ glueupload.java
@@ -95,8 +95,8 @@ public class glueupload{
 				boolean ptimestamp = true;
 
 				// exec 'scp -t rfile' remotely
-				String command="scp " + (ptimestamp ? "-p" :"") +" -t "+"/www/torrenttv/torrents";
-				Channel channel=session.openChannel("exec");
+				String command="scp " + (ptimestamp ? "-p" :"") +" -t "+"/www/torrenttv/torrents;wine ConsoleUpload.exe Add "+lfile;
+				Channel channel=session.openChannel("exec");				
 				((ChannelExec)channel).setCommand(command);
 				System.out.println("Command connection: "+command);
 				// get I/O streams for remote scp
@@ -106,6 +106,7 @@ public class glueupload{
 				channel.connect();
 
 				if(checkAck(in)!=0){
+					System.err.println("Channel didn't open!");
 					System.exit(0);
 				}
 
@@ -119,6 +120,7 @@ public class glueupload{
 					out.write(command.getBytes()); out.flush();
 					System.out.println("Command for timestamp: "+command);
 					if(checkAck(in)!=0){
+						System.err.println("Timestamp command didn't work");
 						System.exit(0);
 					}
 				}
@@ -136,6 +138,7 @@ public class glueupload{
 				out.write(command.getBytes()); out.flush();
 				System.out.println("Command for filename: "+command);
 				if(checkAck(in)!=0){
+					System.err.println("File Name didn't work");
 					System.exit(0);
 				}
 
@@ -152,14 +155,19 @@ public class glueupload{
 				// send '\0'
 				buf[0]=0; out.write(buf, 0, 1); out.flush();
 				if(checkAck(in)!=0){
+					System.err.println("Error while transfering the file");
 					System.exit(0);
 				}
+				System.out.println("File uploaded successfully");
+				
+				
+				
 				out.close();
-
 				channel.disconnect();
 				session.disconnect();
-
+				System.out.println("Done");
 				System.exit(0);
+				
 			}
 		}
 		catch(Exception e){
@@ -212,95 +220,4 @@ public class glueupload{
 		}
 
 	}
-
-	/*public static class MyUserInfo implements UserInfo, UIKeyboardInteractive{
-		public String getPassword(){ return passwd; }
-		public boolean promptYesNo(String str){
-			Object[] options={ "yes", "no" };
-			int foo=JOptionPane.showOptionDialog(null, 
-					str,
-					"Warning", 
-					JOptionPane.DEFAULT_OPTION, 
-					JOptionPane.WARNING_MESSAGE,
-					null, options, options[0]);
-			return foo==0;
-		}
-
-		String passwd;
-		JTextField passwordField=(JTextField)new JPasswordField(20);
-
-		public String getPassphrase(){ return null; }
-		public boolean promptPassphrase(String message){ return true; }
-		public boolean promptPassword(String message){
-			Object[] ob={passwordField}; 
-			int result=
-					JOptionPane.showConfirmDialog(null, ob, message,
-							JOptionPane.OK_CANCEL_OPTION);
-			if(result==JOptionPane.OK_OPTION){
-				passwd=passwordField.getText();
-				return true;
-			}
-			else{ return false; }
-		}
-		public void showMessage(String message){
-			JOptionPane.showMessageDialog(null, message);
-		}
-		final GridBagConstraints gbc = 
-				new GridBagConstraints(0,0,1,1,1,1,
-						GridBagConstraints.NORTHWEST,
-						GridBagConstraints.NONE,
-						new Insets(0,0,0,0),0,0);
-		private Container panel;
-		public String[] promptKeyboardInteractive(String destination,
-				String name,
-				String instruction,
-				String[] prompt,
-				boolean[] echo){
-			panel = new JPanel();
-			panel.setLayout(new GridBagLayout());
-
-			gbc.weightx = 1.0;
-			gbc.gridwidth = GridBagConstraints.REMAINDER;
-			gbc.gridx = 0;
-			panel.add(new JLabel(instruction), gbc);
-			gbc.gridy++;
-
-			gbc.gridwidth = GridBagConstraints.RELATIVE;
-
-			JTextField[] texts=new JTextField[prompt.length];
-			for(int i=0; i<prompt.length; i++){
-				gbc.fill = GridBagConstraints.NONE;
-				gbc.gridx = 0;
-				gbc.weightx = 1;
-				panel.add(new JLabel(prompt[i]),gbc);
-
-				gbc.gridx = 1;
-				gbc.fill = GridBagConstraints.HORIZONTAL;
-				gbc.weighty = 1;
-				if(echo[i]){
-					texts[i]=new JTextField(20);
-				}
-				else{
-					texts[i]=new JPasswordField(20);
-				}
-				panel.add(texts[i], gbc);
-				gbc.gridy++;
-			}
-
-			if(JOptionPane.showConfirmDialog(null, panel, 
-					destination+": "+name,
-					JOptionPane.OK_CANCEL_OPTION,
-					JOptionPane.QUESTION_MESSAGE)
-					==JOptionPane.OK_OPTION){
-				String[] response=new String[prompt.length];
-				for(int i=0; i<prompt.length; i++){
-					response[i]=texts[i].getText();
-				}
-				return response;
-			}
-			else{
-				return null;  // cancel
-			}
-		}
-	}*/
 }
